4730d1506f42980fb01a7fe3496a33e1cac8b75b
Valentin Konusov <rlng-krsk@yandex.ru>
2016-06-04 15:03:07 +0700

    initial commit

---
+++ environments/prod/tests/codeception/config/config-local.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=mysql_test;dbname=gelato_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+    ],
+];
